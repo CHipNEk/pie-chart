fd358ddeb531ef00a78f2187dea1b6fdf4e6bada
Bull.ljftz <125334328+CHipNEk@users.noreply.github.com>
2024-09-19 08:27:48 +0700

    update ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class LuckyWheelComponent {
   slices: number = 1;
   tempSlices: number = 1;
   chartData: any[] = [];
-  colors: string[] = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884d8', '#82ca9d', '#ffc658', '#8dd1e1', '#a4de6c', '#d0ed57', '#ffa726', '#ff7043'];
+  colors: string[] = ['#996aeb', '#dece97', '#e38ab1'];
   chartForm: FormGroup; 
 
   constructor(private fb: FormBuilder) {
@@ -52,26 +52,38 @@ export class LuckyWheelComponent {
   }
 
   createSlicePath(index: number): { path: string, labelPosition: { x: number, y: number } } {
-    const startAngle = index * (360 / this.slices);
-    const endAngle = (index + 1) * (360 / this.slices);
-    const start = this.polarToCartesian(startAngle);
-    const end = this.polarToCartesian(endAngle);
-    const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
-    const path = `M ${start.x} ${start.y} A 100 100 0 ${largeArcFlag} 1 ${end.x} ${end.y} L 0 0`;
-  
-    const midAngle = (startAngle + endAngle) / 2;
-    const labelRadius = 75; 
-    const labelPosition = this.polarToCartesian(midAngle, labelRadius);
+    const startAngle = index * (360 / this.slices); // Góc cắt ban đầu theo index.Chia vòng tròn thành các phần đều nhau
+    const endAngle = (index + 1) * (360 / this.slices); // Góc cắt kết thúc
+    const start = this.polarToCartesian(startAngle); // Nhận vào góc bắt đầu trả về tọa độ x,y
+    const end = this.polarToCartesian(endAngle); // Nhận vào góc kết thúc trả về tọa độ x,y
+    const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1"; // Xác định cung nhỏ (0) hoặc cung lớn (1)
+    const path = `
+                  M ${start.x} ${start.y} 
+                  A 100 100 0 ${largeArcFlag} 1 ${end.x} ${end.y} 
+                  L 0 0
+                `;
+                // M: move to, di chuyển điểm bắt đầu từ tọa độ {start.x} {start.y} 
+                /* A: vẽ cung tròn 
+                      100 100 là bán kính x,y của elip
+                      0 là góc xoay của elip
+                      1 là sweep flag: hướng vẽ cung(1 cho cùng chiều kim đồng hồ) 
+                      ${end.x} ${end.y} điểm kết thúc
+                */
+                // L: line to, vẽ đường thẳng về 0 0(gốc tọa độ)
+
+    const midAngle = (startAngle + endAngle) / 2; // Góc giữa lát cắt
+    const labelRadius = 75; // Bán kính từ tâm đến chỗ đặt text (3/4R)
+    const labelPosition = this.polarToCartesian(midAngle, labelRadius); // Nhận vào góc giữa và bán kính trả về tọa độ x,y
   
     return { path, labelPosition };
   }
   
-  polarToCartesian(angle: number, radius: number = 100): { x: number, y: number } {
-    const radians = (angle - 90) * Math.PI / 180; 
-    return {
-      x: radius * Math.cos(radians),
-      y: radius * Math.sin(radians)
-    };
+  polarToCartesian(angle: number, radius: number = 100): { x: number, y: number } { // Nhận vào góc và bán kính trả về tọa độ x,y
+    const radians = (angle - 90) * Math.PI / 180; // Công thức chuyển đổi từ độ sang radian (radians = degrees * π/180) -90 độ để góc cắt bắt đầu từ đỉnh
+      return { // Trả về tọa độ x,y
+        x: radius * Math.cos(radians),
+        y: radius * Math.sin(radians)
+      };
   }
 
   updateSliceInputs() {
